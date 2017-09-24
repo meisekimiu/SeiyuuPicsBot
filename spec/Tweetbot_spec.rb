bcbc29bdbd2ada6ef77bb99167d44d5c46cb2497
derP@765pro <Giratina493@mew151.net>
2017-09-23 19:54:35 -0700

    Read from config file

--- spec/Tweetbot_spec.rb
+++ spec/Tweetbot_spec.rb
@@ -1,8 +1,19 @@
 require 'rspec'
 require_relative '../src/Tweetbot'
 describe Tweetbot do
+  before :each do
+    @bot = Tweetbot.new (File.expand_path(File.dirname(__FILE__))+"/TestBot.json")
+  end
 
-  it 'should be creatable' do
-    bot = Tweetbot.new
+  describe '#new' do
+    it 'should be creatable' do
+      expect(@bot).to be_an_instance_of Tweetbot
+    end
+  end
+
+  describe '#name' do
+    it 'should get the bot name from configuration' do
+      expect(@bot.name).to eq "TestBot"
+    end
   end
 end
--- src/Tweetbot.rb
+++ src/Tweetbot.rb
@@ -1,3 +1,13 @@
+require 'json'
 class Tweetbot
+  def initialize(path)
+    if File.file?(path)
+      bot_data = JSON.parse(File.read(path), object_class: OpenStruct)
+      @name = bot_data.name
+    end
+  end
 
+  def name
+    return @name
+  end
 end
