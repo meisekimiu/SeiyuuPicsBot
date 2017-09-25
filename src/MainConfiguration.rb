450a9394fa8825d4e43d8f2ec713318880372af8
derP@765pro <Giratina493@mew151.net>
2017-09-25 15:46:20 -0700

    Usage function

--- src/MainConfiguration.rb
+++ src/MainConfiguration.rb
@@ -1,7 +1,15 @@
-def usage
+require_relative 'parameter_parser/KeyValueParameter'
+def usage(defined_parameters)
   STDERR.puts "usage: #{$PROGRAM_NAME} [options] config_file"
-  STDERR.puts "\tconfig_file: The JSON formatted bot configuration."
-  STDERR.puts ""
-  STDERR.puts "See the `config.default.json` file included with the source for a basic config template."
+  defined_parameters.reverse.each do |param|
+	descriptor = "  #{param.isNamed ? param.getSymbol.to_s : param.getIdentifiers.join(', ')}"
+	if descriptior.is_a?(KeyValueParameter)
+	  descriptor += ' <val>'
+	end
+	descriptor += "\t#{param.getDescription}"
+	STDERR.puts descriptor
+  end
+  STDERR.puts ''
+  STDERR.puts 'See the `config.default.json` file included with the source for a basic config template.'
   exit
 end
